142f7f0f269111a632431645f3b71a116d250e6e
Luiy15 <lmaga024@ucr.edu>
2024-01-06 19:49:21 -0800

    Working webscraper for rolex watches using ebay

--- Webscrappers/TempEbay/ebay_WebScraper/ebay_WebScraper/spiders/ebay.py
+++ Webscrappers/TempEbay/ebay_WebScraper/ebay_WebScraper/spiders/ebay.py
@@ -1,18 +1,24 @@
 import scrapy
+import scraper_api
+
+client = scraper_api.ScraperAPIClient('69ec9c37d459d0fd1cee61beaa6ee1ea')
 
 
 class EbaySpider(scrapy.Spider):
     name = "ebay"
-    allowed_domains = ["www.ebay.com"]
+    #allowed_domains = ["www.ebay.com"]
     start_urls = ["https://www.ebay.com/sch/i.html?_from=R40&_nkw=rolex&_sacat=0&rt=nc&Year%2520Manufactured=2020%252DNow&_dcat=31387&_ipg=240"]
 
     def parse(self, response):
         LinkToProducts = response.xpath('//a[@class="s-item__link"]/@href').getall()
         if LinkToProducts:
-            yield from response.follow_all(LinkToProducts,callback=self.parse_page)
+            for url in LinkToProducts: 
+                yield scrapy.Request(client.scrapyGet(url), callback = self.parse_page)
+            #yield from response.follow_all(LinkToProducts,callback=self.parse_page)
         NextPageButton = response.xpath('//a[@aria-label="Go to next search page"]/@href').get()
         if NextPageButton:
-            yield response.follow(NextPageButton, callback=self.parse)
+            yield scrapy.Request(client.scrapyGet(NextPageButton), callback = self.parse)
+            #yield response.follow(NextPageButton, callback=self.parse)
         
         
     def parse_page(self, response):
@@ -23,3 +29,10 @@ class EbaySpider(scrapy.Spider):
         for container in Item_Specs: 
             label = ''.join(container.xpath('.//text()').getall())
             value_ = ''.join(container.xpath('./following-sibling::div[1][@class="ux-labels-values__values"]//text()').getall())
+            all_spec.append(label + ' : ' + value_)
+        all_spec = ' --- ' .join(all_spec)
+        
+        yield{'Product_Title': Product_Title, 
+              'Product_Price': Product_Price, 
+              'Item_Specs': Item_Specs, 
+              'all_spec': all_spec}
